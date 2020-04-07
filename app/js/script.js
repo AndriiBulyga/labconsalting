7c326aea25912e1e634b64de0fdc6765ac5bb439
Andrii Bulyha <andrononemillion@gmail.com>
2020-04-07 17:06:46 +0300

    Проект в норме. Начинаю галерею и слайдер

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
+    // Бургер меню
     $('.navigation__burger').click(function(event) {
         $('.navigation__burger,.navigation__menu').toggleClass('active');
         $('.body').toggleClass('lock');
     });
+    // Слайдер
+    $('.big-slider').slick({
+        autoplay: false,
+        autoplaySpeed: 1000,
+        prewArrow: '<i class="fa fa-angle-left"></i>'
+    });
+    
 });
 
